6f0927e529059c9ece3bc389fc9231654434549b
Jzj <946232976@qq.com>
2024-08-02 17:07:04 +0800

    开启流程控制-v1

--- service-base/src/main/java/com/jzj/base/web/service/impl/ProcessTemplateServiceImpl.java
+++ service-base/src/main/java/com/jzj/base/web/service/impl/ProcessTemplateServiceImpl.java
@@ -1,18 +1,30 @@
 package com.jzj.base.web.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jzj.base.utils.result.BusinessException;
+import com.jzj.base.utils.sign.SecurityUtils;
+import com.jzj.base.web.mapper.ProcessMapper;
 import com.jzj.base.web.mapper.ProcessTemplateMapper;
+import com.jzj.base.web.pojo.entity.Process;
 import com.jzj.base.web.pojo.entity.ProcessTemplate;
+import com.jzj.base.web.pojo.entity.SysUser;
 import com.jzj.base.web.pojo.vo.ProcessFormVo;
 import com.jzj.base.web.service.ProcessService;
 import com.jzj.base.web.service.ProcessTemplateService;
+import com.jzj.base.web.service.SysUserService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * 审批模板Service业务层处理
@@ -22,11 +34,23 @@ import java.util.List;
  */
 @Service
 public class ProcessTemplateServiceImpl extends ServiceImpl<ProcessTemplateMapper,ProcessTemplate> implements ProcessTemplateService {
+
     @Autowired
     private ProcessTemplateMapper processTemplateMapper;
 
     @Autowired
+    private SysUserService sysUserService;
+
+    @Autowired
     private ProcessService processService;
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    private TaskService taskService;
+    @Autowired
+    private ProcessMapper processMapper;
 
     /**
      * 查询审批模板
@@ -116,6 +140,53 @@ public class ProcessTemplateServiceImpl extends ServiceImpl<ProcessTemplateMappe
      */
     @Override
     public void startUp(ProcessFormVo form) {
+        SysUser user = sysUserService.selectByUserName(SecurityUtils.getUserName());
+        if(user==null) throw new BusinessException("用户不存在!");
+        ProcessTemplate template = processTemplateMapper.selectById(form.getProcessTemplateId());
+        Process process = new Process();
+        BeanUtils.copyProperties(form, process);
+        String workNo = System.currentTimeMillis() + "";
+        process.setProcessCode(workNo);
+        process.setUserId(user.getId());
+        process.setFormValues(form.getFormValues());
+        process.setTitle(user.getName() + "发起" + template.getName() + "申请");
+        process.setStatus("1");
+        processService.save(process);
+        //绑定业务id
+        String businessKey = String.valueOf(process.getId());
+        //流程参数
+        Map<String, Object> variables = new HashMap<>();
+        //将表单数据放入流程实例中
+        JSONObject jsonObject = JSON.parseObject(process.getFormValues());
+        JSONObject formData = jsonObject.getJSONObject("formData");
+        Map<String, Object> map = new HashMap<>(formData);
+        variables.put("data", map);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(
+                template.getProcessDefinitionKey(),
+                businessKey,
+                variables);
+        //业务表关联当前流程实例id
+        String processInstanceId = processInstance.getId();
+        process.setProcessInstanceId(processInstanceId);
+        //计算下一个审批人，可能有多个（并行审批）
+        List<Task> taskList = getCurrentTaskList(processInstanceId);
+        if(!taskList.isEmpty()){
+            List<String> assigneeList = new ArrayList<>();
+            for (Task task : taskList) {
+                //assignee就是你创建流程图的时候填写的值
+                SysUser nextUser = sysUserService.selectByUserName(task.getAssignee());
+                assigneeList.add(nextUser.getName());
+                // TODO 推送消息给下一个审批人
+            }
+            process.setDescription("等待" + StringUtils.join(assigneeList.toArray(), ",") + "审批");
+        }
+        processMapper.updateById(process);
+    }
 
+    /**
+     * 获取当前任务列表
+     */
+    private List<Task> getCurrentTaskList(String processInstanceId){
+        return taskService.createTaskQuery().processInstanceId(processInstanceId).list();
     }
 }
